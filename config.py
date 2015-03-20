06211aa6569a14b1bc62115acea7b6e68ac6f897
Qianjunye <f673488649@163.com>
2015-03-20 16:50:08 +0800

    Create new file

---
+++ config.py
@@ -0,0 +1,9 @@
+#-*- encoding: utf-8 -*-
+#配置公私钥
+public_key  = "ucloudxxxxxxx"
+private_key = "3baxxxxxxxxxx"
+#配置检查所在可用区
+Region = "cn-north-03"
+base_url    = "https://api.ucloud.cn"
+Master	=	""
+slaver	=	""
