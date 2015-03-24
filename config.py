900292737ad98042aa7178feb824d2a9578eacd4
Qianjunye <f673488649@163.com>
2015-03-24 14:41:52 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,8 @@ private_key = "3baxxxxxxxxxx"
 #配置检查所在可用区
 Region = "cn-north-03"
 base_url    = "https://api.ucloud.cn"
+
+ImageNamePrefix = "UcloudImage" #镜像名前缀，可以随意设置
+#推荐架构：默认空闲时有两台主机在运行，分别为Master与slaver，如只有一台可将两个变量的设置相同
 Master	=	""
 slaver	=	""
