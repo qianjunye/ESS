2092ced33cb3689acaf359794c8488abd47a50ec
Qianjunye <f673488649@163.com>
2015-04-08 20:35:50 +0800

    Update delete_instance.py

--- delete_instance.py
+++ delete_instance.py
@@ -3,12 +3,11 @@
 
 from sdk import UcloudApiClient
 from config import *
-from Junye import *
+from function import *
 import sys
 import time
 import json
 import time
-import subprocess
 
 #高峰期由crontab执行
 
@@ -16,15 +15,29 @@ import subprocess
 if __name__=='__main__':
     #读取FastigiumInstance.txt中的实例ID，批量删除
     #建议U提供强制关机API
+    FastigiumInstanceIDall=[]
     FastigiumInstanceID=[]
     for ln in file('FastigiumInstance.txt','rt'):
-        FastigiumInstanceID.extend(ln.strip().split(' '))
+        FastigiumInstanceIDall.extend(ln.strip().split(' '))
         print("获取ID")
+    for ids in FastigiumInstanceIDall:
+        Count = eval(DescribeUHostInstance(Region,ids))
+        if Count["TotalCount"]==1:
+            FastigiumInstanceID.append(ids)
     for ids in FastigiumInstanceID:
+        print(ids)
+        b=DescribeUHostInstance(Region,ids)
+        print(b)
+        print "is"
         State=eval(DescribeUHostInstance(Region,ids))
+        a=type(State)
+        print(a)
         print(State["UHostSet"][0]["State"])
         if State["UHostSet"][0]["State"] == "Stopped":
             print("Host is stop")
+            a=DeleteUHostInstance(Region,ids)
+            print "删除" + ids
+            print(a)
         else:
             StopUHostInstance(Region,ids)
             print "关闭" + ids
@@ -38,7 +51,8 @@ if __name__=='__main__':
             State=eval(DescribeUHostInstance(Region,ids))
             print(State["UHostSet"][0]["State"])
             a=DeleteUHostInstance(Region,ids)
-            print "删除" + ids
+            #print "删除" + ids
             print(a)
-    #清空主机池
-    subprocess.Popen("echo "" > FastigiumInstance.txt", shell=True)
+    Clear_File = open('FastigiumInstance.txt', 'w')
+    Clear_File.write("")
+    Clear_File.close( )
